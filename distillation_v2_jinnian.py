8bfc483cdf8db7b08fcdaaddf157227ac5d4d06e
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-18 04:28:30 +0800

    debug torch no grad

--- distillation_v2_jinnian.py
+++ distillation_v2_jinnian.py
@@ -270,17 +270,17 @@ def train_one_epoch_intermediate_distill(config, model, model_teacher, criterion
         if dist.get_rank() == 0:
             #print(len(optimizer.param_groups[0]['params'])+len(optimizer.param_groups[1]['params'])) #189
             #print(len(list(model.named_parameters()))) #189
-            print(list(model.named_parameters())[30])
+            print(list(model.named_parameters())[28])
             #print(model.module.features[0].grad)
         
         optimizer.step()
 
         if dist.get_rank() == 0:
-            print(len(optimizer.param_groups[0]['params'])+len(optimizer.param_groups[1]['params']))
+            #print(len(optimizer.param_groups[0]['params'])+len(optimizer.param_groups[1]['params']))
             #print(optimizer, optimizer.param_groups[0])
             #print(model.module)
             #print(model.module.features[0].weight)
-            print(list(model.named_parameters())[30])
+            print(list(model.named_parameters())[28])
             print(model.module.features[0].grad)
         input('paused!')
 
