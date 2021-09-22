51b8841f9d4b7fd5bb0907f38866a5fef64a1d88
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-23 02:34:49 +0800

    organized

--- run_intermediate_distill.sh
+++ run_intermediate_distill.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 1234 main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path datasets/ --teacher trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_prog_0 --train_intermediate --stage 0 --output output/test_inter_prog 
+python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 1234 main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /root/FastBaseline/data/imagenet --teacher /mnt/configblob/users/v-jinnian/swin_distill/trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_prog_0 --train_intermediate --stage 0 --output output/test_inter_prog 
 
 for ((i=1;i<=3;i++)); do
-    python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 1234 main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path datasets/ --teacher trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_prog_$i --resume output/test_inter_prog/test_inter_prog_$((i-1))/ckpt_epoch_1.pth --train_intermediate --stage $i --output output/test_inter_prog 
+    python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 1234 main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /root/FastBaseline/data/imagenet --teacher /mnt/configblob/users/v-jinnian/swin_distill/trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_prog_$i --resume output/test_inter_prog/test_inter_prog_$((i-1))/ckpt_epoch_1.pth --train_intermediate --stage $i --output output/test_inter_prog 
 done
