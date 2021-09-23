276c0074f678d20cfa7ba8b0fab8b1b8b2086f55
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-23 09:03:50 +0800

    update

--- run_intermediate_distill.sh
+++ run_intermediate_distill.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 epoch=0
-output_dir="test_inter_prog"
+output_dir="test_inter_prog_1e4"
 
 
 python3 -m torch.distributed.launch --nproc_per_node 8 --master_port 1234 main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /root/FastBaseline/data/imagenet --teacher /mnt/configblob/users/v-jinnian/swin_distill/trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_prog_0 --train_intermediate --stage 0 --output output/$output_dir
