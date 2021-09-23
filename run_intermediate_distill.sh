3d6d5b6d824c0f445018770ae8b98c1658844d9f
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-23 21:35:13 +0800

    implement minilm

--- run_intermediate_distill.sh
+++ run_intermediate_distill.sh
@@ -10,6 +10,6 @@ teacher="/mnt/configblob/users/v-jinnian/swin_distill/trained_models/swin_large_
 
 python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path $data_path --teacher $teacher --batch-size 128 --tag intermediate --train_intermediate --ar 48 --student_layer_list [11] --teacher_layer_list [23] --total_train_epoch $epoch_num_interm --output $output_dir --base_lr $base_lr
 
-python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path $data_path --teacher $teacher --batch-size 128 --tag pred_loss --resume output/$output_dir/swin_tiny_patch4_window7_224/intermediate/ckpt_epoch_$((epoch_num_interm-1)).pth --resume_weight_only --output $output_dir
+python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path $data_path --teacher $teacher --batch-size 128 --tag pred_loss --resume $output_dir/swin_tiny_patch4_window7_224/intermediate/ckpt_epoch_$((epoch_num_interm-1)).pth --resume_weight_only --output $output_dir
 --total_train_epoch $epoch_num_pred
 
