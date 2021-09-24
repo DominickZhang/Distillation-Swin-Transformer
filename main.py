e4c77cc59a9262b72ad5118541bc5e89ef6fd0dd
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-24 15:32:46 +0800

    debug

--- main.py
+++ main.py
@@ -317,6 +317,9 @@ def train_one_epoch_intermediate(config, model, model_teacher, criterion, data_l
     end = time.time()
 
     for idx, (samples, targets) in enumerate(data_loader):
+        if idx > 10:
+            break
+
         samples = samples.cuda(non_blocking=True)
         targets = targets.cuda(non_blocking=True)
 
