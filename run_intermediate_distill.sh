04dde72606624151ef51831291c027fb5014fbf6
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-24 15:47:00 +0800

    debug

--- run_intermediate_distill.sh
+++ run_intermediate_distill.sh
@@ -21,7 +21,7 @@ teacher_layer_list=$9
 #student_layer_list="[11]"
 #teacher_layer_list="[23]"
 
-python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path $data_path --teacher $teacher --batch-size 128 --tag intermediate --train_intermediate --ar $ar --student_layer_list $student_layer_list --teacher_layer_list $teacher_layer_list --total_train_epoch $epoch_num_interm --output $output_dir --base_lr $base_lr --load_tar --accumulation_steps 2
+python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path $data_path --teacher $teacher --batch-size 64 --tag intermediate --train_intermediate --ar $ar --student_layer_list $student_layer_list --teacher_layer_list $teacher_layer_list --total_train_epoch $epoch_num_interm --output $output_dir --base_lr $base_lr --load_tar --accumulation-steps 2
 
-python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path $data_path --teacher $teacher --batch-size 128 --tag pred_loss --resume $output_dir/swin_tiny_patch4_window7_224/intermediate/ckpt_epoch_$((epoch_num_interm-1)).pth --resume_weight_only --output $output_dir --total_train_epoch $epoch_num_pred --load_tar --accumulation_steps 2
+python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path $data_path --teacher $teacher --batch-size 64 --tag pred_loss --resume $output_dir/swin_tiny_patch4_window7_224/intermediate/ckpt_epoch_$((epoch_num_interm-1)).pth --resume_weight_only --output $output_dir --total_train_epoch $epoch_num_pred --load_tar --accumulation-steps 2
 
