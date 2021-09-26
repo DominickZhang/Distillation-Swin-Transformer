9c609ff106c2bf0d9fd8a5ff1cdcb044d0d95dec
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-26 14:15:12 +0800

    debug

--- run_command_collections.sh
+++ run_command_collections.sh
@@ -1,5 +1,11 @@
-## GPU07
-python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /sdb/imagenet --teacher ~/trained_models/swin_base_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_all --train_intermediate --ar 64 --student_layer_list [11] --teacher_layer_list [23] --total_train_epoch 5
-## PAI
-python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /root/FastBaseline/data/imagenet --teacher /mnt/configblob/users/v-jinnian/swin_distill/trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_all --train_intermediate --ar 48 --student_layer_list [11] --teacher_layer_list [0] 
+#### GPU07
+## run distillation with pred loss only
+ # python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path /sdb/imagenet --teacher ~/trained_models/swin_base_patch4_window7_224_22kto1k.pth --batch-size 128 --tag debug_da_trial_0 
+
+## run distillation with intermediate loss
+# python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /sdb/imagenet --teacher ~/trained_models/swin_base_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_all --train_intermediate --ar 64 --student_layer_list [11] --teacher_layer_list [23] --total_train_epoch 5
+
+
+#### PAI
+# python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /root/FastBaseline/data/imagenet --teacher /mnt/configblob/users/v-jinnian/swin_distill/trained_models/swin_large_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_all --train_intermediate --ar 48 --student_layer_list [11] --teacher_layer_list [0] 
 
