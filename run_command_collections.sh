ae451abda166f5c26256fd73ef9d1aeb942cb6f4
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-26 14:18:24 +0800

    debug

--- run_command_collections.sh
+++ run_command_collections.sh
@@ -3,7 +3,7 @@
  # python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill.yaml --data-path /sdb/imagenet --teacher ~/trained_models/swin_base_patch4_window7_224_22kto1k.pth --batch-size 128 --tag debug_da_trial_0 
 
 ## run distillation with intermediate loss
-# python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /sdb/imagenet --teacher ~/trained_models/swin_base_patch4_window7_224_22kto1k.pth --batch-size 128 --tag test_inter_all --train_intermediate --ar 64 --student_layer_list [11] --teacher_layer_list [23] --total_train_epoch 5
+# python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path /sdb/imagenet --teacher ~/trained_models/swin_base_patch4_window7_224_22kto1k.pth --batch-size 128 --train_intermediate --ar 64 --student_layer_list [11] --teacher_layer_list [23] --total_train_epoch 5 --tag debug_da_trial_0 
 
 
 #### PAI
