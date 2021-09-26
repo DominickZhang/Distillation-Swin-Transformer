d30df2ba908176e02a96d23c19c4daceb8b8bde2
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-26 18:05:42 +0800

    debug

--- debug.py
+++ debug.py
@@ -6,11 +6,15 @@ import os
 import numpy as np
 
 def test_teacher_logits_dist():
-    data_dict = {959: [], 839: []}
+    #data_dict = {959: [], 839: []}
+    data_dict = {0: [], 1: []}
     for epoch in range(300):
         print(epoch)
         data = torch.load('output/swin_tiny_patch4_window7_224/debug_da_trial_11/sample_target_rank_0_epoch_%d.pth'%epoch)
-        target_index = torch.argmax(data['output'], axis=1)
+        target_index = torch.argmax(data['target'], axis=1)
+        data_dict[target_index[0].item()].append(data['output'])
+        data_dict[target_index[1].item()].append(data['output'])
+        '''
         if target_index[0].item() in [979, 940]:
             data_dict[959].append(data['output'][0].tolist())
         else:
@@ -19,6 +23,7 @@ def test_teacher_logits_dist():
             data_dict[959].append(data['output'][1].tolist())
         else:
             data_dict[target_index[1].item()].append(data['output'][1].tolist())
+        '''
     print(len(data_dict[0]), len(data_dict[1]))
     std0 = np.std(np.array(data_dict[0]), axis=0)
     print(std0, np.max(std0), np.min(std0), np.mean(std0))
