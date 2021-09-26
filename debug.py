f4d60bf76d85bca04aa1ab7f914de7903983552f
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-26 15:57:59 +0800

    debug

--- debug.py
+++ debug.py
@@ -5,7 +5,7 @@ import torch
 import os
 
 def main():
-    trial_list = [0, 2]
+    trial_list = [3, 4]
     data = {}
     for trial_id in trial_list:
         folder_name = 'output/swin_tiny_patch4_window7_224/debug_da_trial_%d'%trial_id
