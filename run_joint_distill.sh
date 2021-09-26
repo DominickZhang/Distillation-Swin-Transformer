af8fff6a4eb29096cb70503203e87ade41587652
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-26 23:21:21 +0800

    joint distill

---
+++ run_joint_distill.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+epoch_num=$1
+output_dir=$2
+base_lr=$3
+ar=$4
+data_path=$5
+teacher=$6
+student_layer_list=$7
+teacher_layer_list=$8
+accumulation_steps=$9
+
+python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path $data_path --teacher $teacher --batch-size 64 --tag joint --joint_distill --ar $ar --student_layer_list $student_layer_list --teacher_layer_list $teacher_layer_list --total_train_epoch $epoch_num--output $output_dir --base_lr $base_lr --load_tar --accumulation-steps $accumulation_steps
+
+
