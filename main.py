c90dffe3b3a9cfdcec0972aaee5968ca29109286
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-26 17:29:43 +0800

    debug

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def cal_intermediate_loss(student_attn_list, student_hidden_list,
 def main(config):
     dataset_train, dataset_val, data_loader_train, data_loader_val, mixup_fn = build_loader(config)
 
-    print(data_loader_train, mixup_fn)
+    print(len(data_loader_train), mixup_fn)
     input('debug')
 
     if config.DISTILL.DO_DISTILL:
