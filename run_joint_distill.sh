3499f4d3362546d41be49dadd9d0c213f7191a88
DominickZhang <jinnian.zhang@wisc.edu>
2021-09-27 13:32:10 +0800

    test joint distill

--- run_joint_distill.sh
+++ run_joint_distill.sh
@@ -22,6 +22,6 @@ echo "accumulation_steps ($accumulation_steps) is not supported!"
 exit
 fi
 
-echo "python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path $data_path --teacher $teacher --batch-size $batch_size --tag joint --joint_distill --ar $ar --student_layer_list $student_layer_list --teacher_layer_list $teacher_layer_list --total_train_epoch $epoch_num --output $output_dir --base_lr $base_lr --load_tar --accumulation-steps $accumulation_steps"
+python -m torch.distributed.launch --nproc_per_node 8 --master_port 1234  main.py --do_distill --cfg configs/swin_tiny_patch4_window7_224_distill_intermediate.yaml --data-path $data_path --teacher $teacher --batch-size $batch_size --tag joint --joint_distill --ar $ar --student_layer_list $student_layer_list --teacher_layer_list $teacher_layer_list --total_train_epoch $epoch_num --output $output_dir --base_lr $base_lr --load_tar --accumulation-steps $accumulation_steps
 
 
